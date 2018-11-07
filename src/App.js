89d74cbdb044068d3783d22a6aa89c23372a0e15
seonmin <ismpink@daum.net>
2018-11-07 18:24:59 +0900

    영화 배열로 변경

--- src/App.js
+++ src/App.js
@@ -3,25 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import Movie from './Movie'
 
-const movieTitles = [
-    "Matrix",
-    "Oldboy",
-    "Star Wars"
+const movies = [
+    {
+        title: "Matrix",
+        poster: "http://sasw.or.kr/zbxe/files/attach/images/172/835/436/3089db3ee6b91b2ba0daa8d5a3bfceb8.jpg"
+    },
+    {
+        title: "Oldboy",
+        poster: "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000077/77772/77772_1000.jpg"
+    },
+    {
+        title: "Star Wars",
+        poster: "https://t1.daumcdn.net/movie/73d078e4c3d27c1d5d2240b4981dc94980676be1"
+    }
 ]
-
-const movieImages = [
-    "http://sasw.or.kr/zbxe/files/attach/images/172/835/436/3089db3ee6b91b2ba0daa8d5a3bfceb8.jpg",
-  "http://img.cgv.co.kr/Movie/Thumbnail/Poster/000077/77772/77772_1000.jpg",
-    "https://t1.daumcdn.net/movie/73d078e4c3d27c1d5d2240b4981dc94980676be1"
-    ]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
 
-        <Movie title={movieTitles[0]} poster={movieImages[0]}/>
-        <Movie title={movieTitles[1]} poster={movieImages[1]}/>
+          {movies.map(movie => {
+             return <Movie title={movie.title} poster={movie.poster} />
+          })}
 
       </div>
     );
