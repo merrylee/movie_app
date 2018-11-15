1920093aa1eecf0d1b77014134b9795221a8997c
seonmin <ismpink@daum.net>
2018-11-15 10:22:55 +0900

    Updates

---
+++ precache-manifest.fc47b50bb2be530efedc60daf1c63840.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "462cc3f029b583ac7d9e",
+    "url": "/movie_app/static/js/main.462cc3f0.chunk.js"
+  },
+  {
+    "revision": "1db8d21f2589b9da6f4a",
+    "url": "/movie_app/static/js/1.1db8d21f.chunk.js"
+  },
+  {
+    "revision": "462cc3f029b583ac7d9e",
+    "url": "/movie_app/static/css/main.277dbca5.chunk.css"
+  },
+  {
+    "revision": "9ce24c14de3dd98563aab52adb542e21",
+    "url": "/movie_app/index.html"
+  }
+];
